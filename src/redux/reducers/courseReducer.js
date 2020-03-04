3310b3329d6ff5740417bd590c6669fce872c9f1
= <jheorge@gmail.com>
2020-03-04 12:34:14 -0400

    added redux complete

--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -1,6 +1,7 @@
+import * as types from "../actions/actionTypes";
 export default function courseReducer(state = [], action) {
   switch (action.type) {
-    case "CREATE_COURSE":
+    case types.CREATE_COURSE:
       //state.push(action.course); //don't do this
       return [...state, { ...action.course }];
     default:
